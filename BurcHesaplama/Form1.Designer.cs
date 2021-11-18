4ff01ae4a80e0784d052a781d3fda55e1022a36f
meleksagir <84595150+meleksagir@users.noreply.github.com>
2021-11-18 10:18:52 +0300

    Initial commit

---
+++ BurcHesaplama/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace BurcHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(48, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğum Tarihiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gün:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ay:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Burcunuz:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(109, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(93, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ocak",
+            "Şubat",
+            "Mart",
+            "Nisan",
+            "Mayıs",
+            "Haziran",
+            "Temmuz",
+            "Ağustos",
+            "Eylül",
+            "Ekim",
+            "Kasım",
+            "Aralık"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboBox2.Location = new System.Drawing.Point(84, 57);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(118, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 370);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ BurcHesaplama/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BurcHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string burc;
+            int gun = Convert.ToInt32(comboBox2.Text);
+            string ay = Convert.ToString(comboBox1.Text);
+            if ((gun >= 22 && ay == "Aralık") || (gun <= 20 && ay =="Ocak"))
+            {
+                burc = "Oğlak";
+                textBox3.Text = burc;
+               
+            }
+           if ((gun >= 21 && ay == "Ocak") ||( gun <= 19 && ay == "Şubat"))
+            {
+                burc = "Kova";
+                textBox3.Text = burc;
+            }
+            if ((gun >= 20 && ay == "Şubat") || (gun <= 20 && ay == "Mart"))
+            {
+                burc = "Balık";
+                textBox3.Text = burc;
+            }
+            if ((gun >= 21 && ay == "Mart") || (gun <= 20 && ay == "Nisan"))
+            {
+                burc = "Koç";
+                textBox3.Text = burc;
+            }
+             if ((gun >= 20 && ay == "Nisan") || (gun <= 20 && ay == "Mayıs"))
+            {
+                burc = "Boğa";
+                textBox3.Text = burc;
+            //    pictureBox1.Image = Image.FromFile(C:\\Users\melek.sagir\Pictures\Screenshots);
+            }
+            if ((gun >= 21 && ay == "Mayıs") || (gun <= 21 &&  ay == "Haziran"))
+            {
+                burc = "İkizler";
+                textBox3.Text = burc;
+            }
+            if ((gun >= 23 && ay == "Haziran" )|| (gun <= 22 &&  ay == "Temmuz"))
+            {
+                burc = "Yengeç";
+                textBox3.Text = burc;
+
+            }
+            if ((gun >= 22 && ay == "Temmuz") || (gun <= 23 &&  ay == "Ağustos"))
+            {
+                burc = "Aslan";
+                textBox3.Text = burc;
+            }
+             if ((gun >= 23 && ay == "Ağustos" )||( gun <= 23 && ay == "Eylül"))
+            {
+                burc = "Başak";
+                textBox3.Text = burc;
+            }
+            if ((gun >= 23 && ay == "Eylül") ||( gun <= 23 &&ay == "Ekim"))
+            {
+                burc = "Terazi";
+                textBox3.Text = burc;
+            }
+            if ((gun >= 23 && ay == "Ekim") ||( gun <= 22 && ay == "Kasım"))
+            {
+                burc = "Akrep";
+                textBox3.Text = burc;
+            }
+            if((gun >= 22 && ay == "Kasım") || (gun <= 21 && ay == "Aralık"))
+            {
+                burc = "Yay";
+                textBox3.Text = burc;
+            }
+
+        }
+    }
+}
